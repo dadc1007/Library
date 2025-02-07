02176961085d279fbcf5a0d1ebc1e49c4b058d92
dadc1007 <dadica1007@gmail.com>
2025-02-06 22:38:41 -0500

    Implemente la prueba para addBook

--- src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
+++ src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -1,20 +1,36 @@
 package edu.eci.cvds.tdd.library;
 
-import org.junit.jupiter.api.BeforeAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import edu.eci.cvds.tdd.library.book.Book;
 
 public class LibraryTest {
-    public LibraryTest() {
-        Library library = new Library();
+    private Library library;
+    private Book book1;
+    private Book book2;
+    private Book book3;
+    private Book book4;
+    private Book book5;
+    
+    @BeforeEach
+    public void setUp() {
+        library = new Library();
+        book1 = new Book("El principito", "Antoine de Saint-Exupéry", "978-607-99498-0-8");
+        book2 = new Book("El principito", "Antoine de Saint-Exupéry", "978-849-83814-9-8");
+        book3 = new Book("El principito", "Antoine de Saint-Exupéry", "978-847-88871-9-4");
+        book4 = new Book("cien años de soledad", "Gabriel García Márquez", "978-849-75922-0-8");
+        book5 = new Book("Don Quijote de la Mancha", "Miguel de Cervantes", "978-958-30044-4-5");
     }
 
-    @BeforeAll
-    public void setUp() {
-        Book book1 = new Book("El principito", "Antoine de Saint-Exupéry", "978-607-99498-0-8");
-        Book book2 = new Book("El principito", "Antoine de Saint-Exupéry", "978-849-83814-9-8");
-        Book book3 = new Book("El principito", "Antoine de Saint-Exupéry", "978-847-88871-9-4");
-        Book book4 = new Book("cien años de soledad", "Gabriel García Márquez", "978-849-75922-0-8");
-        Book book5 = new Book("Don Quijote de la Mancha", "Miguel de Cervantes", "978-958-30044-4-5");
+    @Test
+    public void shouldAddBook() {
+        assertTrue(library.addBook(book1));
+        assertTrue(library.addBook(book2));
+        assertTrue(library.addBook(book3));
+        assertTrue(library.addBook(book4));
+        assertTrue(library.addBook(book5));
     }
 }
